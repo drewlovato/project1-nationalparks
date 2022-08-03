7c301c677d1aaa51fa127bfb575c4661ecdb40bf
Rod Bennett <rodbennett@Rods-Air-2.lan>
2022-08-02 20:37:12 -0600

    apis added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,34 @@
-console.log("hello");
+const searchParkEl = document.querySelector('.searchBar')
+const searchBtnEl = document.querySelector('.searchBtn')
+
+searchBtnEl.addEventListener('click', parkWeather)
 
 
+var apiPark = 'dtbgvyHKYoiS5V9y5hZJq49IJEEH16UFSVHhvdbe'
+var apiWeather = "a79cc559d0824f46711db4a217d374a2"
 
-var apiKey = 'dtbgvyHKYoiS5V9y5hZJq49IJEEH16UFSVHhvdbe'
-var parkName = 
 
+function parkName() {
+  fetch (`https://developer.nps.gov/api/v1/parks?parkCode=${searchParkEl.value}&api_key=dtbgvyHKYoiS5V9y5hZJq49IJEEH16UFSVHhvdbe`)
+  .then((response) => response.json())
+  .then ((data) => {
+    console.log(data)
+     const name = data[0].name
+     const parkDesc = data[0].description
+     const lat = data[0].latitude
+     const lon = data[0].longitude
 
-https://developer.nps.gov/api/v1/parks?parkCode=${parkName}&api_key=dtbgvyHKYoiS5V9y5hZJq49IJEEH16UFSVHhvdbe
+     parkWeather(name, parkDesc, lat, lon)
+  })
+}
+
+//parkName()
+
+function parkWeather() {
+fetch (`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hour&units=imperial&appid=a79cc559d0824f46711db4a217d37`)
+    .then ((response) => response.json())
+    .then ((data) => {
+        console.log(data)
+
+    })
+}

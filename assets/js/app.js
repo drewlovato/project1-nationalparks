9ecc497c6640ffe74b52336de60e20e632d60c75
Rod Bennett <rodbennett@Rods-Air-2.lan>
2022-08-02 19:42:56 -0600

    api setup

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,9 @@
 console.log("hello");
+
+
+
+var apiKey = 'dtbgvyHKYoiS5V9y5hZJq49IJEEH16UFSVHhvdbe'
+var parkName = 
+
+
+https://developer.nps.gov/api/v1/parks?parkCode=${parkName}&api_key=dtbgvyHKYoiS5V9y5hZJq49IJEEH16UFSVHhvdbe

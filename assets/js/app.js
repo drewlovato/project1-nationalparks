79bddbf0036dac58a67e9a512804ac40588e47ce
Rod Bennett <rodbennett@Rods-Air-2.lan>
2022-08-04 23:27:41 -0600

    array of parks and codes completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,22 +32,64 @@ let lon = "";
 // array for national park names for autosearch:
 // array for autocomplete
 const allParks = [ 
-  {
-    name: "Acadia National Park", 
-    code: "acad"
-  }, 
-  { 
-    name: "Zion National Park", 
-    code: "zion"
-  },
-
-  {"Arches National Park": "arch"},
-  {"Badlands National Park": "badl"},
-  {"Big Bend National Park": "bibe"}, 
-  {"Biscayne National Park": "bisc"}, 
+  { name: "Acadia National Park", code: "acad" },
+  { name: "Arches National Park", code: "arch" },
+  { name: "Badlands National Park", code: "badl" },
+  { name: "Big Bend National Park", code: "bibe" },
+  { name: "Biscayne National Park", code: "bisc"}, 
+  { name: "Bryce Canyon National Park", code: "brca" },
+  { name: "Canyonlands National Park", code: "cany" },
+  { name: "Capitol Reef National Park", code: "care"},
+  { name: "Carlsbad Caverns National Park", code: "carl" },
+  { name: "Channel Islands National Park", code: "chis"},
+  { name: "Congaree National Park", code: "cong" },
+  { name: "Crater Lake National Park", code: "crla" },
+  { name: "Cuyahoga Valley National Park", code: "cuva"},
+  { name: "Death Valley National Park", code: "deva" },
+  { name: "Denali National Park and Preserve", code: "dena"},
+  { name: "Dry Tortugas National Park", code: "drto" },
+  { name: "Everglades National Park", code: "ever"},
+  { name: "Gates of the Arctic National Park", code: "gaar" },
+  { name: "Gateway Arch National Park", code: "gate" },
+  { name: "Glacier National Park", code: "glac" },
+  { name: "Glacier Bay National Park", code:"glba" },
+  { name: "Grand Canyon National Park", code: "grte"},
+  { name: "Great Sand Dunes National Park and Preserve", code: "grsa" },
+  { name: "Great Smoky Mountains National Park", code: "grsm" },
+  { name: "Guadalupe Mountains National Park", code: "gumo"},
+  { name: "Haleakala National Park", code: "hale" },
+  { name: "Hawaii Volcanoes National Park", code: "havo" },
+  { name: "Hot Springs National Park", code: "hosp"},
+  { name: "Indiana Dunes National Park", code: "indu"},
+  { name: "Isle Royale National Park", code: "isro" },
+  { name: "Joshua Tree National Park", code:"jotr" },
+  { name: "Katmai National Park and Preserve", code:"katm" },
+  { name: "Kenai Fjords National Park", code: "kefj"},
+  { name: "Kobuk Valley National Park", code: "kova" },
+  { name: "Lake Clark National Park", code: "lacl"},
+  { name: "Lassen Volcanic National Park", code: "lavo" },
+  { name: "Mammoth Cave National Park", code: "maca"},
+  { name: "Mesa Verde National Park", code: "meve"},
+  { name: "Mount Rainier National Park", code: "mora" },
+  { name: "New River Gorge National Park", code:"neri" },
+  { name: "North Cascades National Park", code: "noca" },
+  { name: "Olympic National Park", code: "olym" },
+  { name: "Petrified Forest National Park", code: "pefo" },
+  { name: "Pinnacles National Park", code: "pinn" },
+  { name: "Redwood National Park", code: "redw" },
+  { name: "Rocky Mountain National Park", code: "romo" },
+  { name: "Saguaro National Park", code: "sagu" },
+  { name: "Shenandoah National Park", code: "shen" },
+  { name: "Theodore Roosevelt National Park", code: "thro" },
+  { name: "Virgin Islands National Park", code: "viis" },
+  { name: "Voyageurs National Park", code: "voya" },
+  { name: "White Sands National Park", code: "whsa" },
+  { name: "Wind Cave National Park", code: "wica" },
+  { name: "Wrangell-St. Elias National Park and Preserve", code: "wrst" },
+  { name: "Yellowstone National Park", code: "yell" },
+  { name: "Yosemite National Park", code: "yose" },
+  { name: "Zion National Park", code: "zion"},
 ]
-
-  // "Black Canyon of the Gunnison National Park", "Bryce Canyon National Park", "Canyonlands National Park", "Capitol Reef National Park", "Carlsbad Caverns National Park", "Channel Islands National Park", "Congaree National Park", "Crater Lake National Park", "Cuyahoga Valley National Park", "Death Valley National Park", "Denali National Park and Preserve", "Dry Tortugas National Park", "Everglades National Park", "Gates of the Arctic National Park", "Gateway Arch National Park", "Glacier National Park", "Glacier Bay National Park", "Grand Canyon National Park", "Grand Teton National Park", "Great Basin National Park", "Great Sand Dunes National Park and Preserve", "Great Smoky Mountains National Park", "Guadalupe Mountains National Park", "Haleakala National Park", "Hawaii Volcanoes National Park", "Hot Springs National Park", "Indiana Dunes National Park", "Isle Royale National Park", "Joshua Tree National Park", "Katmai National Park and Preserve", "Kenai Fjords National Park", "Kings Canyon National Park", "Kobuk Valley National Park", "Lake Clark National Park", "Lassen Volcanic National Park", "Mammoth Cave National Park", "Mesa Verde National Park", "Mount Rainier National Park", "National Park of American Samoa", "New River Gorge National Park", "North Cascades National Park", "Olympic National Park", "Petrified Forest National Park", "Pinnacles National Park", "Redwood National Park", "Rocky Mountain National Park",  "Saguaro National Park", "Sequoia National Park", "Shenandoah National Park", "Theodore Roosevelt National Park", "Virgin Islands National Park", "Voyageurs National Park", "White Sands National Park", "Wind Cave National Park", "Wrangell-St. Elias National Park and Preserve", "Yellowstone National Park", "Yosemite National Park", "Zion National Park"]
 
 // key events for autocomplete searchbar.  Showing only values types by users.
 inputBox.onkeyup = (e) => {
@@ -105,7 +147,6 @@ function parkName(event) {
   event.preventDefault()
   let parkCode;
   for (let i = 0; i < allParks.length; i++) {
-    console.log((allParks[i].name)) 
     if(searchParkEl.value == (allParks[i].name)) {
      parkCode = allParks[i].code
     }
@@ -116,7 +157,6 @@ function parkName(event) {
   )
     .then((response) => response.json())
     .then((data) => {
-      console.log(data)
       namePark = data.data[0].name;
       parkDesignation = data.data[0].designation;
       parkState = data.data[0].addresses[0].stateCode;
@@ -149,7 +189,6 @@ console.log("hello")
 
       for (let index = 0; index < allEntranceFees.length - 1; index++) {
         const element = allEntranceFees[index];
-        console.log(element);
         let feeCostVal = element.cost;
         let feeDescVal = element.description;
         let feeTitleVal = element.title;

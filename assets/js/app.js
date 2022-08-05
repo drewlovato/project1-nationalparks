9df03bc623514af2bef4318c6ffbbafb8c1d57d0
Rod Bennett <rodbennett@Rods-Air-2.lan>
2022-08-04 22:06:25 -0600

    images and api solved

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 //varibles from html page
 const searchParkEl = document.querySelector(".searchBar");
 const searchBtnEl = document.querySelector(".searchBtn");
+const imageEl = document.querySelector(".park-images")
+let inputBox = document.querySelector("input")
+
 
 //variables for park biogragphy
 const parkBioEl = document.querySelector(".parkBio");
@@ -25,6 +28,69 @@ let parkDesc = "";
 let lat = "";
 let lon = "";
 
+
+// array for national park names for autosearch:
+// array for autocomplete
+const allParks = [ 
+  {
+    name: "Acadia National Park", 
+    code: "acad"
+  }, 
+  { 
+    name: "Zion National Park", 
+    code: "zion"
+  },
+
+  {"Arches National Park": "arch"},
+  {"Badlands National Park": "badl"},
+  {"Big Bend National Park": "bibe"}, 
+  {"Biscayne National Park": "bisc"}, 
+]
+
+  // "Black Canyon of the Gunnison National Park", "Bryce Canyon National Park", "Canyonlands National Park", "Capitol Reef National Park", "Carlsbad Caverns National Park", "Channel Islands National Park", "Congaree National Park", "Crater Lake National Park", "Cuyahoga Valley National Park", "Death Valley National Park", "Denali National Park and Preserve", "Dry Tortugas National Park", "Everglades National Park", "Gates of the Arctic National Park", "Gateway Arch National Park", "Glacier National Park", "Glacier Bay National Park", "Grand Canyon National Park", "Grand Teton National Park", "Great Basin National Park", "Great Sand Dunes National Park and Preserve", "Great Smoky Mountains National Park", "Guadalupe Mountains National Park", "Haleakala National Park", "Hawaii Volcanoes National Park", "Hot Springs National Park", "Indiana Dunes National Park", "Isle Royale National Park", "Joshua Tree National Park", "Katmai National Park and Preserve", "Kenai Fjords National Park", "Kings Canyon National Park", "Kobuk Valley National Park", "Lake Clark National Park", "Lassen Volcanic National Park", "Mammoth Cave National Park", "Mesa Verde National Park", "Mount Rainier National Park", "National Park of American Samoa", "New River Gorge National Park", "North Cascades National Park", "Olympic National Park", "Petrified Forest National Park", "Pinnacles National Park", "Redwood National Park", "Rocky Mountain National Park",  "Saguaro National Park", "Sequoia National Park", "Shenandoah National Park", "Theodore Roosevelt National Park", "Virgin Islands National Park", "Voyageurs National Park", "White Sands National Park", "Wind Cave National Park", "Wrangell-St. Elias National Park and Preserve", "Yellowstone National Park", "Yosemite National Park", "Zion National Park"]
+
+// key events for autocomplete searchbar.  Showing only values types by users.
+inputBox.onkeyup = (e) => {
+  let userData = e.target.value;
+  let emptyArray = [];
+  if(userData) {
+    emptyArray = allParks.filter((data) => {
+      return data.toLocaleLowerCase().startsWith(userData.toLocaleLowerCase());
+    });
+    emptyArray = emptyArray.map((data) => {
+      return data = '<li>' + data + '</li>';
+    });
+    console.log(emptyArray)
+    searchWrapper.classList.add('active')
+  } else {
+    searchWrapper.classList.remove('active')
+  }
+  showSugggestions(emptyArray);
+  let allList = suggBox.querySelectorAll("li")
+  for (let i = 0; i < allList.length; i++) {
+    allList[i].setAttribute("onclick", "select(this)")
+  }
+} 
+
+// putting user selected list item in textarea
+function select(element){
+  let selectUserData = element.textContent
+  inputBox.value = selectUserData
+  searchWrapper.classList.remove('active')
+}
+// showing autoSuggest list
+function showSugggestions(list) {
+  let listData;
+  if(!list.length){
+    userValue = inputBox.value;
+  }else {
+    listData = list.join('');
+    listData = '<li>' + userValue + "</li>";
+  }
+  suggBox.innerHTML = listData
+}
+
+
 // api key for national parks
 let apiPark = "dtbgvyHKYoiS5V9y5hZJq49IJEEH16UFSVHhvdbe";
 //api key of openweather
@@ -36,19 +102,41 @@ searchBtnEl.addEventListener("click", parkName);
 
 // function 1 - runs national parks api
 function parkName(event) {
-
   event.preventDefault()
+  let parkCode;
+  for (let i = 0; i < allParks.length; i++) {
+    console.log((allParks[i].name)) 
+    if(searchParkEl.value == (allParks[i].name)) {
+     parkCode = allParks[i].code
+    }
+    
+  }
   fetch(
-    `https://developer.nps.gov/api/v1/parks?parkCode=${searchParkEl.value}&api_key=dtbgvyHKYoiS5V9y5hZJq49IJEEH16UFSVHhvdbe`
+    `https://developer.nps.gov/api/v1/parks?parkCode=${parkCode}&api_key=dtbgvyHKYoiS5V9y5hZJq49IJEEH16UFSVHhvdbe`
   )
     .then((response) => response.json())
     .then((data) => {
+      console.log(data)
       namePark = data.data[0].name;
       parkDesignation = data.data[0].designation;
       parkState = data.data[0].addresses[0].stateCode;
       parkDesc = data.data[0].description;
       lat = data.data[0].latitude;
       lon = data.data[0].longitude;
+
+      // park photos from API 
+
+      image1 = data.data[0].images[0].url
+      image2 = data.data[0].images[1].url
+      image3 = data.data[0].images[2].url
+      image4 = data.data[0].images[3].url
+console.log("hello")
+      let imageArray = [image1, image2, image3, image4]
+      for (let i = 0; i < imageArray.length; i++) {
+        const slideShow = imageArray[i];
+        imageEl.setAttribute('src', slideShow)
+        
+      }
 
       //START OF 4 ENTRANCE FEES
       // setting variable for each fee
@@ -59,7 +147,7 @@ function parkName(event) {
 
       let allEntranceFees = [fee1, fee2, fee3, fee4];
 
-      for (let index = 0; index < allEntranceFees.length; index++) {
+      for (let index = 0; index < allEntranceFees.length - 1; index++) {
         const element = allEntranceFees[index];
         console.log(element);
         let feeCostVal = element.cost;
